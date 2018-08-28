b292e6f39f7e618cc0dd6ebd8771cac14e77f946
ColletteMarie <collettemariep@gmail.com>
2018-08-28 12:36:47 +0100

    Add JS code for new financial calculator pages

--- app/routes.js
+++ app/routes.js
@@ -17,30 +17,108 @@ router.get('/', function (req, res) {
 router.get('/accomodationType', function (req, res) {
   var option = req.query.accomodationType
   if (option === 'flat-house') {
-    res.redirect('savings-and-investment')
+    res.redirect('financial-checker/savings-and-investment')
   }
   else {
-    res.render('where-do-you-live')
+    res.render('financial-checker/where-do-you-live')
   }
 })
+
+// savings and investments
 
 router.get('/savingsValue', function (req, res) {
   var option = req.query.savingsValue
   if (option === 'over') {
-    res.redirect('self-funded-care')
+    res.redirect('financial-checker/self-funded-care')
   }
-  // if (option === 'between') {
-  //   res.redirect('private-pension')
-  // }
   else {
-    res.render('savings-and-investment')
+    res.render('financial-checker/age')
   }
 })
+
+// age
+
+router.get('/howOld', function (req, res) {
+  var option = req.query.howOld
+  if (option === 'eighteen') {
+    res.redirect('financial-checker/benefits')
+  }
+  else if (option === 'twentyfour') {
+    res.redirect('financial-checker/benefits')
+  }
+  else {
+    res.render('financial-checker/private-pensions')
+  }
+})
+
+
+// pensions
+
+router.get('/privatePension', function (req, res) {
+  var option = req.query.privatePension
+  if (option === 'yes') {
+      res.redirect('financial-checker/benefits')
+    }
+  else {
+    res.render('financial-checker/state-pensions')
+  }
+})
+
+
+router.get('/statePension', function (req, res) {
+  var option = req.query.statePension
+  if (option === 'yes') {
+    res.redirect('financial-checker/state-pensions')
+  }
+  else {
+    res.render('financial-checker/benefits')
+  }
+})
+
+// benefits
+
+router.get('/benefits', function (req, res) {
+  var option = req.query.benefits
+  if (option === 'yes') {
+    res.redirect('financial-checker/benefits')
+  }
+  else {
+    res.render('financial-checker/rent-property')
+  }
+})
+
+
+// rent out property
+
+router.get('/rentProperty', function (req, res) {
+  var option = req.query.rentProperty
+  if (option === 'yes') {
+    res.redirect('financial-checker/rent-property')
+  }
+  else {
+    res.render('financial-checker/other-income')
+  }
+})
+
+
+// other income
+
+router.get('/otherIncome', function (req, res) {
+  var option = req.query.otherIncome
+  if (option === 'yes') {
+    res.redirect('financial-checker/other-income')
+  }
+  else {
+    res.render('financial-checker/self-funded-care')
+  }
+})
+
 
 // what-financial-assessment
 
 
 module.exports = router
+
 
 
 
