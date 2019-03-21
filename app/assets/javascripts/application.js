bc9f6d1573afb0d42edaf0a7c073005f562b9be1
Martin J Thompson <martinjthompson@Lucias-MacBook-Pro.local>
2019-03-21 16:27:05 +0000

    add text update for console output

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,6 @@
-/* global $ */
-/* global GOVUK */
-
-// Warn about using the kit in production
+// Console message of development stage
 if (window.console && window.console.info) {
-  window.console.info('GOV.UK Prototype Kit - do not use for production')
+  window.console.info('essex.gov.uk beta')
 }
 
 $(document).ready(function () {
